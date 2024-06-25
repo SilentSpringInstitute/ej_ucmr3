022737aa35bfa22db97013205edacca1e3b8db7d
aaron-maruzzo <maruzzo@silentspring.org>
2024-06-24 21:13:51 -0700

    analysis in progress

--- 7.1 water sys in UCMR versus SDWIS.R
+++ 7.1 water sys in UCMR versus SDWIS.R
@@ -1,3 +1,154 @@
+sdwis2013 <- read_xlsx("raw/Water System Detail_20240624.xlsx")
+
+colnames <- as.character(as.vector(sdwis2013[4,]))
+colnames <- str_to_lower(colnames)
+colnames <- str_replace_all(colnames, " ", "_")
+colnames
+
+sdwis2013 <- sdwis2013[-(1:4),]
+colnames(sdwis2013) <- colnames
+
+sys_in_ucmr3 <- unique(ucmr3_raw$PWSID)
+length(sys_in_ucmr3) #5401 water systems
+
+sys_in_ucmr3 <- unique(ucmr_detcode$PWSID)
+length(sys_in_ucmr3) #4815 water systems <- USE THIS
+
+sdwis2013 %>%
+  mutate(in_ucmr3 = if_else(pws_id %in% sys_in_ucmr3, "yes", "no")) %>%
+  tabyl(in_ucmr3)
+
+sdwis2013 %>%
+  mutate(in_ucmr3 = if_else(pws_id %in% sys_in_ucmr3, "yes", "no")) %>%
+  group_by(in_ucmr3) %>% 
+  summarise(total_pop_served = sum(as.numeric(population_served_count))) %>%
+  ungroup() %>%
+  mutate(total_pop = sum(total_pop_served), 
+         freq = 100*total_pop_served/total_pop)
+
+# size 
+
+# systems in SDWIS
+size1 <- sdwis2013 %>% 
+  mutate(size_cat = case_when(
+    as.numeric(population_served_count) > 10000 ~ "L", 
+    as.numeric(population_served_count) > 3300 & 
+      as.numeric(population_served_count) <= 10000 ~ "S", 
+    as.numeric(population_served_count) >= 0 & 
+      as.numeric(population_served_count) <= 3300 ~ "verySm",
+    TRUE ~ "oops"
+  ))
+
+# check 1: no missing size categories
+stopifnot(nrow(filter(size1, size_cat == "oops"))==0)
+
+# systems in the paper
+size2 <- ucmr_detcode %>% distinct(PWSID, Size)
+
+size3 <- size1 %>%
+  left_join(
+    size2 %>% 
+      mutate(in_ucmr3 = "yes") %>%
+      rename(size_in_paper = Size), 
+    by = c("pws_id" = "PWSID")
+  ) %>%
+  # if a system is in size2 but not in size1, then that system was 
+  # not in the ucmr3 paper and merged with a blank ("NA")
+  mutate(in_ucmr3 = if_else(is.na(in_ucmr3), "no", in_ucmr3))
+
+size4 <- size3 %>% 
+  mutate(same_or_different = case_when(
+    in_ucmr3 == "yes" & size_cat == size_in_paper ~ "same", 
+    in_ucmr3 == "yes" & size_cat != size_in_paper ~ "different", 
+    in_ucmr3 == "no" ~ "-9999", 
+    TRUE ~ "oops"
+  )) 
+
+size4 %>% 
+  filter(in_ucmr3 == "yes") %>% 
+  tabyl(same_or_different)
+
+size4 %>%
+  filter(same_or_different == "different") %>%
+  count(size_cat, size_in_paper)
+
+size3 %>%
+  filter(size_cat == "L") %>%
+  filter(primacy_type == "State") %>%
+  tabyl(in_ucmr3) %>%
+  adorn_totals()
+
+size3 %>% 
+  filter(size_cat == "S") %>%
+  tabyl(in_ucmr3)
+
+size1 %>% tabyl(size_cat)
+
+# source water type 
+
+unique(sdwis2013$primary_source)
+
+source1 <- sdwis2013 %>%
+  mutate(pws_type = case_when(
+    primary_source == "Ground water" ~ "GW", 
+    primary_source == "Ground water purchased" ~ "GW",
+    primary_source == "Surface water purchased" ~ "SW", 
+    primary_source == "Surface water" ~ "SW", 
+    primary_source == "Unknown Primary Source" ~ "UNK", 
+    primary_source == "Groundwater under influence of surface water" ~ "MIX", 
+    primary_source == "Purchased ground water under influence of surface water source" ~ "MIX", 
+    TRUE ~ "oops"
+  ))
+
+source1 %>% count(primary_source, pws_type) 
+
+source1 %>% count(pws_type) %>% mutate(total = sum(n), freq = 100*n/total)
+
+source2 <- ucmr_detcode %>% distinct(PWSID, source_type)
+
+source3 <- source1 %>%
+  select(pws_id, primary_source, pws_type) %>%
+  left_join(source2 %>% 
+              mutate(in_ucmr3 = "yes") %>%
+              rename(pws_type_in_ucmr3 = source_type), 
+            by = c("pws_id" = "PWSID"))
+
+source4 <- source3 %>% 
+  mutate(in_ucmr3 = if_else(is.na(in_ucmr3), "no", in_ucmr3)) %>%
+  mutate(same_or_different = case_when(
+    in_ucmr3 == "yes" & pws_type == pws_type_in_ucmr3 ~ "same", 
+    in_ucmr3 == "yes" & pws_type != pws_type_in_ucmr3 ~ "different", 
+    in_ucmr3 == "no" ~ "-9999", 
+    TRUE ~ "oops"
+  )) 
+
+source4 %>% 
+  filter(in_ucmr3 == "yes") %>% 
+  tabyl(same_or_different)
+
+source4 %>% 
+  filter(in_ucmr3 == "yes") %>% 
+  filter(same_or_different == "different") %>%
+  count(primary_source, pws_type_in_ucmr3)
+
+sys_id_small <- size4 %>% filter(size_cat == "S") %>% pull(pws_id) %>% unique()
+length(sys_id_small) # 5,157 systems are small
+100*length(sys_id_small) / nrow(size4)
+
+source5 <- source4 %>%
+  filter(pws_id %in% sys_id_small) %>%
+  count(in_ucmr3, pws_type) %>%
+  group_by(in_ucmr3) %>%
+  mutate(total = sum(n), 
+         freq = 100*n/total) %>%
+  ungroup()
+
+source5 %>%
+  mutate(n = paste0(n, " (", round(freq,1), ")")) %>%
+  pivot_wider(id_cols = pws_type, names_from = in_ucmr3, values_from = n)
+
+table(source2$source_type)
+
 library(janitor)
 
 allsdwis_dc %>% 
