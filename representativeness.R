53ea3e724848d756711f97037c359f5437249f0c
aaron-maruzzo <maruzzo@silentspring.org>
2024-07-01 08:38:59 -0700

    scratch paper on pws representativeness

--- representativeness.R
+++ representativeness.R
@@ -5,38 +5,12 @@
 # Start here ------------------------------------------------------------------
 # 
 # If not already in the working environment, run:
-# source("3. analyze ucmr3 - bivar comparisons (t-tests, Exact test).R")
+# source("3. analyze ucmr3 - bivar comparisons (t-tests, Exact tests).R")
 # to identify which counties were included in the paper
 
 library(tidyverse)
 library(patchwork)
 library(janitor)
-
-# Water systems ---------------------------------------------------------------
-
-# Data sources: 
-# Active PWSs in 2013 Q4 
-# Active and inactive PWSs in 2013 Q4
-# Our main dataset (dat_clean)
-
-# https://echo.epa.gov/tools/data-downloads/sdwa-download-summary
-
-# Systems = Active (A), Changed from public to non-public (N), Merged with another system (M)
-
-sdwis2013 <- read_xlsx("raw/Water System Detail_20240624.xlsx")
-
-colnames <- as.character(as.vector(sdwis2013[4,]))
-colnames <- str_to_lower(colnames)
-colnames <- str_replace_all(colnames, " ", "_")
-colnames
-
-sdwis2013 <- sdwis2013[-(1:4),]
-colnames(sdwis2013) <- colnames
-
-dim(sdwis2013)
-str(sdwis2013)
-
-unique(sdwis2013$activity_status)
 
 # Counties --------------------------------------------------------------------
 
@@ -46,6 +20,11 @@ county_ids_vec
 str(cn15)
 # check if cn15 has one line per county.
 stopifnot(nrow(cn15) == nrow(distinct(cn15)))
+
+cn15 %>% 
+  filter(GEO.id2 %in% county_ids_vec) %>%
+  mutate(any_urbanized = if_else(perc_urban > 0, "yes", "no")) %>%
+  tabyl(any_urbanized)
 
 cn15 %>% 
   mutate(in_paper = if_else(GEO.id2 %in% county_ids_vec, "yes", "no")) %>%
@@ -67,11 +46,15 @@ cn15 %>%
     iqr_urban = IQR(perc_urban)
   )
 
+
+
 make_county_plot <- function(demo){
+  
   cn15 %>%
   mutate(in_paper = if_else(GEO.id2 %in% county_ids_vec, "yes", "no")) %>%
-    group_by(in_paper) %>%
+  group_by(in_paper) %>%
   mutate(in_paper = paste0(in_paper, "\nN=", length(in_paper))) %>%
+  ungroup() %>% 
   ggplot(aes(x = in_paper, y = !!enquo(demo))) + 
   geom_jitter(shape = 1, 
               color = "grey75") +
@@ -85,16 +68,70 @@ make_county_plot <- function(demo){
                colour = "red", 
                size = 4,
                geom = "text", 
-               aes(label = after_stat(y)),
-               position = position_nudge(x = 0.4, y = 5)) + 
+               aes(label = after_stat(round(y, 1))),
+               position = position_nudge(x = 0.1, y = 10)) + 
   scale_y_continuous(limits = c(0, 100), expand = c(0, 0)) + 
+  labs(x = "") + 
   theme_bw() +
   theme(title = element_text(size = 9))
 }
 
-p1 <- make_county_plot(demo = perc_hisp_any) + labs(x = "", y = "Percent", title = "Percent Hispanic")
-p2 <- make_county_plot(demo = perc_black_nohisp)  + labs(x = "", y = "", title = "Percent NH Black")
-p3 <- make_county_plot(demo = perc_urban) + labs(x = "", y = "", title = "Percent urban")
-p4 <- make_county_plot(demo = mdi_rate) + labs(x = "", y = "", title = "Percent deprived")
+p1 <- make_county_plot(demo = perc_hisp_any) + labs(y = "Percent", title = "Percent Hispanic")
+p2 <- make_county_plot(demo = perc_black_nohisp)  + labs(y = "", title = "Percent NH Black")
+p3 <- make_county_plot(demo = perc_urban) + labs(y = "", title = "Percent urban")
+p4 <- make_county_plot(demo = mdi_rate) + labs(y = "", title = "Percent deprived")
 
-p1 + p2 + p3 + p4 + plot_layout(nrow = 1)
+axis_title <- ggplot(data.frame(x = c(0, 1)), aes(x = x)) + geom_blank() +
+  theme_void() + 
+  theme(axis.title.x = element_text()) + 
+  labs(x = "County linked to a UCMR 3 water system")
+
+plot <- (p1 + p2 + p3 + p4) + plot_layout(nrow = 1) 
+plot <- patchworkGrob(plot)
+x.label <- grid::textGrob("County linked to a UCMR 3 water system", vjust = -1)
+gridExtra::grid.arrange(plot, bottom = x.label)
+
+# PWS types in the UCMR 3 -----------
+
+sys_in_ucmr3 <- dat_ucmr3 %>% filter(!is.na(det_pfas)) %>% pull(PWSID)
+length(sys_in_ucmr3) #4920
+sdwis2013 <- read_excel("raw/Water System Detail_20240624.xlsx")
+
+clean_colnames <- sdwis2013[4,] %>% pivot_longer(cols = everything()) %>% pull(value)
+clean_colnames <- str_to_lower(clean_colnames)
+clean_colnames <- str_replace_all(clean_colnames, " ", "_")
+
+sdwis2013_clean <- sdwis2013[-c(1:4),]
+colnames(sdwis2013_clean) <- clean_colnames
+sdwis2013_clean
+
+dat_clean %>% 
+  tabyl(size) %>%
+  adorn_totals()
+
+colnames(allsdwis3)
+
+sdwis2013_clean %>% 
+  filter(pws_id %in% sys_in_ucmr3) %>%
+  nrow()
+  
+allsdwis3 %>% 
+  filter(PWSID %in% sys_in_ucmr3) %>%
+  nrow()
+
+sdwis2013_clean %>% 
+  filter(pws_id %in% sys_in_ucmr3) %>%
+  mutate(size = if_else(population_served_count >= 10000, "L", "S")) %>%
+  count(pws_type)
+
+allsdwis3 %>% 
+  filter(PWSID %in% dat_clean$PWSID) %>%
+  mutate(size = if_else(WS.POPULATION_SERVED_COUNT >= 10000, "L", "S")) %>%
+  count(size, PWS_TYPE_CODE)
+
+summary(dat_clean$perc_urban)
+hist(dat_clean$perc_urban)
+
+dat_clean %>%
+  mutate(is_zero = if_else(perc_urban == 0, 'yes', 'no')) %>% 
+  count(is_zero)
