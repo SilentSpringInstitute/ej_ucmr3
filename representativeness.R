ea8cb3f4987ff465b3effdc09ae59e5296092dc7
aaron-maruzzo <maruzzo@silentspring.org>
2024-07-11 19:03:51 -0700

    more notes

--- representativeness.R
+++ representativeness.R
@@ -2,8 +2,10 @@
 # AM 
 # Compare representative-ness 
 
-# Start here ------------------------------------------------------------------
-# 
+#===============================================================================
+# start here
+#===============================================================================
+
 # If not already in the working environment, run:
 # source("3. analyze ucmr3 - bivar comparisons (t-tests, Exact tests).R")
 # to identify which counties were included in the paper
@@ -14,22 +16,17 @@ library(janitor)
 
 # Counties --------------------------------------------------------------------
 
+# census data 
 cn15 
-county_ids_vec 
+# vector of counties linked to a UCMR 3 
+length(county_ids_vec) #1718
 
-str(cn15)
 # check if cn15 has one line per county.
 stopifnot(nrow(cn15) == nrow(distinct(cn15)))
 
-cn15 %>% 
-  filter(GEO.id2 %in% county_ids_vec) %>%
-  mutate(any_urbanized = if_else(perc_urban > 0, "yes", "no")) %>%
-  tabyl(any_urbanized)
-
-cn15 %>% 
-  mutate(in_paper = if_else(GEO.id2 %in% county_ids_vec, "yes", "no")) %>%
-  tabyl(in_paper) %>%
-  adorn_totals()
+# calculate the median and IQR for perc hispanic, perc black, perc urban, and perc deprived
+# between counties that were linked to a UCMR 3 (n=1718) versus not linked 
+# (n=1424)
 
 cn15 %>% 
   mutate(in_paper = if_else(GEO.id2 %in% county_ids_vec, "yes", "no")) %>%
@@ -46,10 +43,17 @@ cn15 %>%
     iqr_urban = IQR(perc_urban)
   )
 
+#===============================================================================
+# plot 
+#===============================================================================
 
+## this function outputs a plot of counties stratified by whether they were 
+## linked to a UCMR 3 system (x-axis) and % demographic. overlays a red line 
+## and value corresponding to the median value in % demographic. 
+## users assign a demographic value ("demo"), which refers to a column in "cn15" 
+## data
 
 make_county_plot <- function(demo){
-  
   cn15 %>%
   mutate(in_paper = if_else(GEO.id2 %in% county_ids_vec, "yes", "no")) %>%
   group_by(in_paper) %>%
@@ -76,24 +80,27 @@ make_county_plot <- function(demo){
   theme(title = element_text(size = 9))
 }
 
+# make plots
 p1 <- make_county_plot(demo = perc_hisp_any) + labs(y = "Percent", title = "Percent Hispanic")
 p2 <- make_county_plot(demo = perc_black_nohisp)  + labs(y = "", title = "Percent NH Black")
 p3 <- make_county_plot(demo = perc_urban) + labs(y = "", title = "Percent urban")
 p4 <- make_county_plot(demo = mdi_rate) + labs(y = "", title = "Percent deprived")
 
+# define axis titles and axes
 axis_title <- ggplot(data.frame(x = c(0, 1)), aes(x = x)) + geom_blank() +
   theme_void() + 
   theme(axis.title.x = element_text()) + 
   labs(x = "County linked to a UCMR 3 water system")
 
+# combine plots together
 plot <- (p1 + p2 + p3 + p4) + plot_layout(nrow = 1) 
 plot <- patchworkGrob(plot)
 x.label <- grid::textGrob("County linked to a UCMR 3 water system", vjust = -1)
 gridExtra::grid.arrange(plot, bottom = x.label)
 
-# PWS types in the UCMR 3 -----------
+# # PWS types in the UCMR 3 -----------
 # 
-# sys_in_ucmr3 <- dat_ucmr3 %>% filter(!is.na(det_pfas)) %>% pull(PWSID)
+# sys_in_ucmr3 <- dat_clean %>% filter(!is.na(det_pfas)) %>% pull(PWSID)
 # length(sys_in_ucmr3) #4920
 # sdwis2013 <- read_excel("raw/Water System Detail_20240624.xlsx")
 # 
@@ -105,26 +112,27 @@ gridExtra::grid.arrange(plot, bottom = x.label)
 # colnames(sdwis2013_clean) <- clean_colnames
 # sdwis2013_clean
 # 
-# dat_clean %>% 
+# dat_clean %>%
 #   tabyl(size) %>%
 #   adorn_totals()
 # 
 # colnames(allsdwis3)
 # 
-# sdwis2013_clean %>% 
+# sdwis2013_clean %>%
 #   filter(pws_id %in% sys_in_ucmr3) %>%
 #   nrow()
-#   
-# allsdwis3 %>% 
-#   filter(PWSID %in% sys_in_ucmr3) %>%
-#   nrow()
 # 
-# sdwis2013_clean %>% 
+# allsdwis3 %>%
+#   filter(PWSID %in% sys_in_ucmr3) %>%
+#   count(PWS_TYPE_CODE) %>%
+#   mutate(freq = 100*n/sum(n))
+# 
+# sdwis2013_clean %>%
 #   filter(pws_id %in% sys_in_ucmr3) %>%
 #   mutate(size = if_else(population_served_count >= 10000, "L", "S")) %>%
 #   count(pws_type)
 # 
-# allsdwis3 %>% 
+# allsdwis3 %>%
 #   filter(PWSID %in% dat_clean$PWSID) %>%
 #   mutate(size = if_else(WS.POPULATION_SERVED_COUNT >= 10000, "L", "S")) %>%
 #   count(size, PWS_TYPE_CODE)
@@ -133,5 +141,5 @@ gridExtra::grid.arrange(plot, bottom = x.label)
 # hist(dat_clean$perc_urban)
 # 
 # dat_clean %>%
-#   mutate(is_zero = if_else(perc_urban == 0, 'yes', 'no')) %>% 
+#   mutate(is_zero = if_else(perc_urban == 0, 'yes', 'no')) %>%
 #   count(is_zero)
